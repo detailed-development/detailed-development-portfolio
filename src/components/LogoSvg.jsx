068e19d263387b95d1be452c82b524b437b50d64
anthøny <anthony@detailed-development.com>
2026-06-10 15:28:32 -0700

    Refactor SVG paths and apply transformations

--- src/components/LogoSvg.jsx
+++ src/components/LogoSvg.jsx
@@ -12,19 +12,26 @@ export default function LogoSvg(props) {
           <stop offset="0%" stopColor="#ffffff" />
           <stop offset="100%" stopColor="#d4d4d4" />
         </radialGradient>
+    
         <filter id={shadowId}>
-          <feDropShadow dx="0" dy="2" stdDeviation="4" floodColor="#000000" floodOpacity="0.5" />
+          <feDropShadow
+            dx="0"
+            dy="2"
+            stdDeviation="4"
+            floodColor="#000000"
+            floodOpacity="0.5"
+          />
         </filter>
       </defs>
-      <path
-        d="M20 10h40c27.6 0 50 22.4 50 50s-22.4 50-50 50H20V10z"
-        fill={`url(#${gradientId})`}
-        filter={`url(#${shadowId})`}
-      />
-      <path
-        d="M38 30h22c16.6 0 30 13.4 30 30s-13.4 30-30 30H38V30z"
-        fill="var(--bg)"
-      />
+    
+      <g transform="scale(0.46875)">
+        <path
+          fill={`url(#${gradientId})`}
+          filter={`url(#${shadowId})`}
+          fillRule="evenodd"
+          d="M53 35h73c50 0 88 40 88 93s-38 92-88 92H53V35Zm25 20v145h45c39 0 66-30 66-72s-27-73-66-73H78Zm13 20h41c22 0 37 22 37 53s-15 53-37 53H91v-60h20v41h13c15 0 25-14 25-34s-10-34-25-34h-13v6H91V75Z"
+        />
+      </g>
     </svg>
   )
 }
